619d150b01dbedd09409bfb73f9e8a0501b0c1a8
McChikenz <cyrilszekiel.aranas@gmail.com>
2026-07-17 15:10:52 -0400

    Update folder Structure

---
+++ part2/build/CMakeFiles/PointTriangle.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PointTriangle.dir/src/main.cpp.o"
+  "CMakeFiles/PointTriangle.dir/src/main.cpp.o.d"
+  "CMakeFiles/PointTriangle.dir/src/point.cpp.o"
+  "CMakeFiles/PointTriangle.dir/src/point.cpp.o.d"
+  "CMakeFiles/PointTriangle.dir/src/triangle.cpp.o"
+  "CMakeFiles/PointTriangle.dir/src/triangle.cpp.o.d"
+  "PointTriangle"
+  "PointTriangle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PointTriangle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
